f02b2edd0d8a1bcd682424a670aa108940a63f91
mschulist <mschulist2@gmail.com>
2021-11-05 21:57:10 -0700

    Finished Order

--- Derivatives/src/Derivatives.R
+++ Derivatives/src/Derivatives.R
@@ -5,14 +5,14 @@ library(here)
 library(XML)
 library(ggpubr)
 
-#Importing Data
-point_one_molar_xml <- xmlToDataFrame(here("Derivatives/input/0.10_NaOH.cmbl"))
-point_one_molar_xml_parse <- xmlParse(here("Derivatives/input/0.10_NaOH.cmbl"))
-point_one_molar_list <- xmlToList(point_one_molar_xml_parse)
-point_one_molar_time <- read_table(point_one_molar[["DataSet"]][["DataColumn"]][["ColumnCells"]],col_names = FALSE)
-point_one_molar_absorbance <- read_table(point_one_molar[["DataSet"]][["DataColumn"]][["ColumnCells"]],col_names = FALSE)
+#Importing Data (Doesn't work because Logger Pro has stupid formatting - uses the same name for different columns)
+#point_one_molar_xml <- xmlToDataFrame(here("Derivatives/input/0.10_NaOH.cmbl"))
+#point_one_molar_xml_parse <- xmlParse(here("Derivatives/input/0.10_NaOH.cmbl"))
+#point_one_molar_list <- xmlToList(point_one_molar_xml_parse)
+#point_one_molar_time <- read_table(point_one_molar[["DataSet"]][["DataColumn"]][["ColumnCells"]],col_names = FALSE)
+#point_one_molar_absorbance <- read_table(point_one_molar[["DataSet"]][["DataColumn"]][["ColumnCells"]],col_names = FALSE)
 
-#Boring way to import data
+#Boring way to import data **Needed for order**
 point_one_molar <- read_csv(here("Derivatives/input/0.10_M_NaOH.csv"))
 point_zero_five_molar <- read_csv(here("Derivatives/input/0.05_M_NaOH.csv"))
 
@@ -23,7 +23,7 @@ ggplot()+
   geom_smooth(data = point_one_molar,aes(x=time,y=absorbance),color="purple")+
   geom_smooth(data = point_zero_five_molar,aes(x=time,y=absorbance),color="green")
 
-#Finding the Initial Rate (est. using first 4 points)
+#Getting first 4 points (est. using first 4 points) **Needed for order**
 initial_point_one_molar <- point_one_molar[1:4,]
 initial_point_zero_five_molar <- point_zero_five_molar[1:4,]
 
@@ -43,6 +43,22 @@ ggplot()+
     color = "Legend"
   )
 
-#Getting the numbers
-lm(initial_point_one_molar$absorbance~initial_point_one_molar$time) %>% 
-  [["coefficients"]][["initial_point_one_molar$time"]] -> initial_rate_point_one_molar
+#Getting the numbers **Needed for order**
+initial_list_point_one_molar <- lm(initial_point_one_molar$absorbance~initial_point_one_molar$time) 
+initial_rate_point_one_molar <- abs(initial_list_point_one_molar[["coefficients"]][["initial_point_one_molar$time"]])
+
+initial_list_point_zero_five_molar <- lm(initial_point_zero_five_molar$absorbance~initial_point_zero_five_molar$time)
+initial_rate_point_zero_five_molar <- abs(initial_list_point_zero_five_molar[["coefficients"]][["initial_point_zero_five_molar$time"]])
+
+#Putting the rates into a table **Needed for order**
+
+rates <- c(initial_rate_point_one_molar,initial_rate_point_zero_five_molar)
+molarity <- c("0.10","0.05")
+rate_table <- data.frame(as.numeric(molarity),as.numeric(rates))
+colnames(rate_table) <- c("molarity","rate")
+
+#Finally getting the order **Needed for order**
+
+order <- round((rate_table[1,1]/rate_table[2,1])/(rate_table[1,2]/rate_table[2,2]))
+print(order)
+
