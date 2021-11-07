a77e674e3b846607b3c0b7ed5731a1024d5da866
mschulist <mschulist2@gmail.com>
2021-11-07 08:30:09 -0800

    Cleaned up environment

--- Derivatives/src/Derivatives.R
+++ Derivatives/src/Derivatives.R
@@ -6,11 +6,11 @@ library(XML)
 library(xml2)
 library(ggpubr)
 
-#File Locations
+#File Locations **Needed for order**
 point_one_molar_cmbl <- here("Derivatives/input/0.10_NaOH.cmbl")
 point_zero_five_molar_cmbl <- here("Derivatives/input/0.05_NaOH.cmbl")
 
-#Importing Data
+#Importing Data **Needed for order**
 point_one_molar_read <- read_xml(point_one_molar_cmbl)
 point_zero_five_molar_read <- read_xml(point_zero_five_molar_cmbl)
 #Sort by the Column Cells (the data we care about in the XML file)
@@ -21,12 +21,12 @@ point_one_molar_time <- xmlParse(point_one_molar_columns[[1]])
 point_one_molar_absorbance <- xmlParse(point_one_molar_columns[[2]])
 point_zero_five_molar_time <- xmlParse(point_zero_five_molar_columns[[1]])
 point_zero_five_molar_absorbance <- xmlParse(point_zero_five_molar_columns[[2]])
-#Convert the nodes into vectors
+#Convert the nodes into string with "\n" delim
 point_one_molar_time_list <- xmlToList(point_one_molar_time)
 point_one_molar_absorbance_list <- xmlToList(point_one_molar_absorbance)
 point_zero_five_molar_time_list <- xmlToList(point_zero_five_molar_time)
 point_zero_five_molar_absorbance_list <- xmlToList(point_zero_five_molar_absorbance)
-#Get rid of the "\n" delim
+#Get rid of the "\n" delim and create vector
 point_one_molar_time_read <- read_csv(point_one_molar_time_list,col_names = FALSE)
 point_one_molar_absorbance_read <- read_csv(point_one_molar_absorbance_list,col_names = FALSE)
 point_zero_five_molar_time_read <- read_csv(point_zero_five_molar_time_list,col_names = FALSE)
@@ -36,12 +36,13 @@ colnames(point_one_molar_time_read) <- "time"
 colnames(point_one_molar_absorbance_read) <- "absorbance"
 colnames(point_zero_five_molar_time_read) <- "time"
 colnames(point_zero_five_molar_absorbance_read) <- "absorbance"
-#Make the vectors into a data.frame
+#Make the vectors into a data.frame (tibble)
 point_one_molar <- tibble(point_one_molar_time_read,point_one_molar_absorbance_read)
 point_zero_five_molar <- tibble(point_zero_five_molar_time_read,point_zero_five_molar_absorbance_read)
+#Clean up (get rid of objects no longer needed)
+rm(list=ls()[! ls() %in% c("point_one_molar","point_zero_five_molar")])
 
-
-#Boring way to import data **Needed for order** (Fixed with XML Imports)
+#Boring way to import data (Fixed with XML Imports)
 #point_one_molar <- read_csv(here("Derivatives/input/0.10_M_NaOH.csv"))
 #point_zero_five_molar <- read_csv(here("Derivatives/input/0.05_M_NaOH.csv"))
 
@@ -80,7 +81,6 @@ initial_list_point_zero_five_molar <- lm(initial_point_zero_five_molar$absorbanc
 initial_rate_point_zero_five_molar <- abs(initial_list_point_zero_five_molar[["coefficients"]][["initial_point_zero_five_molar$time"]])
 
 #Putting the rates into a table **Needed for order**
-
 rates <- c(initial_rate_point_zero_five_molar,initial_rate_point_one_molar)
 molarity <- c("0.05","0.10")
 rate_table <- data.frame(as.numeric(molarity),as.numeric(rates))
@@ -88,7 +88,6 @@ colnames(rate_table) <- c("molarity","rate")
 rate_table <- arrange(rate_table,molarity)
 
 #Finally getting the order **Needed for order**
-
 order <- log(round(rate_table[2,2]/rate_table[1,2]))/log(rate_table[2,1]/rate_table[1,1])
 print(order)
 
